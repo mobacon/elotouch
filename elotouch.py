69b9314e83a01df4f2ed48ec609c9987e858f2ce
Sven Schlender <svesch@gmx.de>
2015-11-16 19:16:40 +0100

    Completed 10 byte status message parsing.

--- elotouch.py
+++ elotouch.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
 import serial
-import uinput
+#import uinput
 import argparse
 import logging
-#from verbosity import (argparse_verbosity, configure_verbosity)
 
 DEFAULT_BAUDRATE = 9600
 
-ELO10_LEAD_BYTE = 'U'
-ELO10_TOUCH_PACKET = 'T'
-ELO10_PRESSURE = 0x80
+ELO10_LEAD_BYTE = 0x55    # 'U'
+ELO10_TOUCH_PACKET = 0x54 # 'T'
 ELO10_TOUCH = 0x03
+
+ELO_DATA = None
 
 
 def parse_arguments():
@@ -33,7 +33,50 @@ def parse_arguments():
     return parser.parse_args()
 
         
-ELO_DATA = None
+
+def argparse_verbosity(parser, default=0):
+    parser.add_argument('-v', "--verbose", dest="verbose", action="count",
+                        default=default,
+                        help="Print out debug and info messages.")
+    parser.add_argument('--logfile', dest='logfile', required=False, 
+                        type=str, default=None, 
+                        help='Give a log file pathname.')                        
+    parser.add_argument('--stdout', dest='sout', required=False, 
+                        action="store_true", default=False, 
+                        help='Output log to standard out.')
+
+
+def configure_verbosity(args, fmt = '%(message)s'):
+    """Configure verbosity for logging.
+    
+    @param args Parsed arguments from command line.
+    @param fmt How the output looks like.
+    """    
+    if not args.verbose:
+        log_level = logging.ERROR
+    else:
+        if args.verbose == 1:
+            log_level = logging.WARNING
+        elif args.verbose == 2:
+            log_level = logging.INFO
+        else:
+            log_level = logging.DEBUG
+
+    if args.logfile:
+        logging.basicConfig(format=fmt, level=log_level, filename=args.logfile, 
+                            filemode="a")
+    else:
+        logging.basicConfig(format=fmt, level=log_level)
+
+    if args.sout:
+        root = logging.getLogger()
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(log_level)
+        formatter = logging.Formatter(fmt)
+        ch.setFormatter(formatter)
+        root.addHandler(ch)
+
+        
         
 def elo_process_data_10(data):
     global ELO_DATA, ELO_CSUM
@@ -43,51 +86,62 @@ def elo_process_data_10(data):
             logging.warning("Got unsynchronized data: 0x%02x\n" % data)
             return
         else:
+            logging.debug("Got lead")
             ELO_CSUM = 0xaa
-            ELO_DATA = bytearray(data)
+            ELO_DATA = bytearray([data])
     else:
         ELO_DATA.append(data)
 
     if len(ELO_DATA) != 10:
-        ELO_CSUM += data
+        ELO_CSUM = (ELO_CSUM + data) & 0xFF
         return
-        
+    
     # Process complete message....
     if data != ELO_CSUM:
         logging.warning("bad checksum: 0x%02x, expected 0x%02x", data, ELO_CSUM);
         return
     if ELO_DATA[1] == ELO10_TOUCH_PACKET:
+        status = ELO_DATA[2]
         abs_x = (ELO_DATA[4] << 8) | ELO_DATA[3]
         abs_y = (ELO_DATA[6] << 8) | ELO_DATA[5]
-        logging.info("got touch packet, abs_x=%d, abs_y=%d", abs_x, abs_y)
+        if status & ELO10_TOUCH:
+            touch = True
+        else:
+            touch = False
+        abs_z = (ELO_DATA[8] << 8) | ELO_DATA[7]
+            
+        logging.info("got touch packet, status=0x%02x, touch=%d, abs_x=%d, abs_y=%d, abs_z=%d", status, touch, abs_x, abs_y, abs_z)
         
-        if ELO_DATA[2] & ELO10_PRESSURE:
+        #if status & ELO10_PRESSURE:
             #input_report_abs(dev, ABS_PRESSURE, (elo->data[8] << 8) | elo->data[7]);
             #input_report_key(dev, BTN_TOUCH, elo->data[2] & ELO10_TOUCH);
             #input_sync(dev);
-            pass
-        ELO_DATA = None
+            
+    ELO_DATA = None
     
             
 def main():
     args = parse_arguments()
-    #configure_verbosity(args)   
+    configure_verbosity(args)   
     
     try:
-        with serial.Serial(args.port, baudrate=args.baud, timeout=.5) as eport:
+        with serial.Serial(args.port, baudrate=args.baud, rtscts=True, timeout=.5) as eport:
             logging.info("Port %s successfully opened!", args.port)
             
             while True:
                 data = eport.read(1)
                 if not len(data):
                     continue
-                elo_process_data_10(data)
+                data = ord(data)
                 
-                    
-            
-            
-    except serial.SerialException, KeyboardInterrupt:
+                elo_process_data_10(data)
+                # ToDo: Add also 6 and 4 byte processing.
+         
+    except KeyboardInterrupt:
         pass
+    except serial.SerialException as e:
+        logging.error(e)
+        exit(-1)
 
 
 if __name__ == '__main__':
